c40f66ca8a1b07ed875176438750e6852a142cda
Ondrej Rafaj <pro@Super-Dictator.local>
2017-08-31 22:50:33 +0100

    adding CORS

--- backend/Sources/App/Controllers/BumpsController.swift
+++ backend/Sources/App/Controllers/BumpsController.swift
@@ -21,72 +21,10 @@ enum BumpsError: Error {
 final class BumpsController: Controller {
     
     
-    static func buildNumber(request: Request, drop: Droplet) throws -> ResponseRepresentable {
-        let token = try authenticate(request: request, drop: drop)
-        
-        if let plain = request.query?["plain"]?.bool, plain == true {
-            return String(token.build)
-        }
-        else {
-            return token
-        }
-    }
-    
-    static func incrementBuildNumber(request: Request, drop: Droplet) throws -> ResponseRepresentable {
-        let token = try authenticate(request: request, drop: drop)
-        
-        if let currentBuild = request.query?["build"]?.int, token.build < currentBuild {
-            token.build = (currentBuild + 1)
-        }
-        else {
-            token.build += 1
-        }
-        try token.save()
-        
-        if let plain = request.query?["plain"]?.bool, plain == true {
-            return String(token.build)
-        }
-        else {
-            return token
-        }
-    }
-    
-    static func token(request: Request, drop: Droplet) throws -> ResponseRepresentable {
-        guard let bundleId: String = request.query?["bundle"]?.string, bundleId.characters.count > 0 else {
-            throw BumpsError.missingBundleId
-        }
-        
-        var app = try App.find(bundleId: bundleId)
-        if app == nil {
-            app = App.init(bundle: bundleId, added: Date())
-            try app?.save()
-        }
-        
-        let uuid = UUID().uuidString
-        let hashedToken = try drop.hash.make(uuid).makeString()
-        
-        let token = Token(token: hashedToken, build: 0, appId: app!.id!, added: Date())
-        try token.save()
-        token.token = uuid
-        
-        if let plain = request.query?["plain"]?.bool, plain == true {
-            return token.token
-        }
-        else {
-            return try token.makeFullJSON()
-        }
-    }
-    
-}
-
-
-extension BumpsController {
-    
-    
     static func cors(request: Request) -> ResponseRepresentable {
         let response = Response(status: .noContent)
-        response.headers["Content-Type"] = "application/json"
-        response.headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS"
+        response.headers["Content-Type"] = "application/json; charset=utf-8"
+        response.headers["Access-Control-Allow-Methods"] = "GET,OPTIONS"
         if let origin: String = request.headers["Origin"] {
             response.headers["Access-Control-Allow-Origin"] = origin
             response.headers["Access-Control-Expose-Headers"] = "Authorization"
@@ -108,6 +46,65 @@ extension BumpsController {
         }
         response.headers["Access-Control-Allow-Headers"] = headers.joined(separator: ",")
         response.headers["Access-Control-Max-Age"] = "400"
+        return response
+    }
+    
+    static func buildNumber(request: Request, drop: Droplet) throws -> ResponseRepresentable {
+        let token = try authenticate(request: request, drop: drop)
+        return try output(request: request, tokenData: token.makeJSON(), plainText: String(token.build))
+    }
+    
+    static func incrementBuildNumber(request: Request, drop: Droplet) throws -> ResponseRepresentable {
+        let token = try authenticate(request: request, drop: drop)
+        
+        if let currentBuild = request.query?["build"]?.int, token.build < currentBuild {
+            token.build = (currentBuild + 1)
+        }
+        else {
+            token.build += 1
+        }
+        try token.save()
+        
+        return try output(request: request, tokenData: token.makeJSON(), plainText: String(token.build))
+    }
+    
+    static func token(request: Request, drop: Droplet) throws -> ResponseRepresentable {
+        guard let bundleId: String = request.query?["bundle"]?.string, bundleId.characters.count > 0 else {
+            throw BumpsError.missingBundleId
+        }
+        
+        var app = try App.find(bundleId: bundleId)
+        if app == nil {
+            app = App.init(bundle: bundleId, added: Date())
+            try app?.save()
+        }
+        
+        let uuid = UUID().uuidString
+        let hashedToken = try drop.hash.make(uuid).makeString()
+        
+        let token = Token(token: hashedToken, build: 0, appId: app!.id!, added: Date())
+        try token.save()
+        token.token = uuid
+        
+        return try output(request: request, tokenData: token.makeFullJSON(), plainText: String(token.token))
+    }
+    
+}
+
+
+extension BumpsController {
+    
+    
+    fileprivate static func output(request: Request, tokenData: JSON, plainText: String) throws -> Response {
+        let response = Response(status: .created)
+        if let plain = request.query?["plain"]?.bool, plain == true {
+            response.body = Body(plainText)
+            response.headers["Content-Type"] = "text/plain; charset=utf-8"
+        }
+        else {
+            response.body = try Body(tokenData)
+            response.headers["Content-Type"] = "application/json; charset=utf-8"
+        }
         return response
     }
     
